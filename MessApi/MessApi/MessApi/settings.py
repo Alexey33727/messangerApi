513e6e88855ffd797b5b111424e851e35088b916
Alexey33727 <108624817+Alexey33727@users.noreply.github.com>
2023-04-29 12:59:54 +1000

    v3
    ds

--- MessApi/MessApi/MessApi/settings.py
+++ MessApi/MessApi/MessApi/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-8zcs!l^-^9zq0#&)8mu)cxdi-9ol5!375rba5g*zh%w#rv70g)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
